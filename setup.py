274c7021185857c32cef9c6f575d388ff3c893e9
Alexander Eryomin <alneremin@gmail.com>
2026-03-06 15:59:17 +0300

    ! move to colcon

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
-from catkin_pkg.python_setup import generate_distutils_setup
-
 from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['genpy'],
-    package_dir={'': 'src'},
-    requires=['genmsg']
-)
+package_name = 'genpy'
 
-setup(**d)
+setup(
+    name=package_name,
+    version='0.19.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Mikael Arguedas',
+    author_email='mikael@osrfoundation.org',
+    maintainer='Aditya Pande, Alejandro Hernandez Cordero',
+    maintainer_email='aditya.pande@openrobotics.org, alejandro@openrobotics.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Examples of minimal subscribers using rclpy.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'genpy = genpy.genpy_main:genmain'
+        ],
+    },
+)
